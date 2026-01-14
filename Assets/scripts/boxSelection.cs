edcf699c126ed020bb1aded51ee2f8bf12e53d06
H00gy <148908869+H00gy@users.noreply.github.com>
2026-01-14 16:01:40 -0500

    fixed the damn bug

--- Assets/scripts/boxSelection.cs
+++ Assets/scripts/boxSelection.cs
@@ -14,40 +14,51 @@ public class boxSelection : MonoBehaviour
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 0;
+        /*
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, Vector3.zero);
+        lineRenderer.SetPosition(1, Vector3.right * 5f);
+        */
     }
 
     private void Update()
     {
         // when left mouse button is pressed and  mouse pointer is not over any object
         // I create four points at mouse position
+        
         if (Input.GetMouseButtonDown(0) && !ObjectSelected.mouseOverObject)
         {
-            lineRenderer.positionCount = 4;
+            lineRenderer.positionCount = 5;
             initialMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             lineRenderer.SetPosition(0, new Vector2(initialMousePosition.x, initialMousePosition.y));
             lineRenderer.SetPosition(1, new Vector2(initialMousePosition.x, initialMousePosition.y));
             lineRenderer.SetPosition(2, new Vector2(initialMousePosition.x, initialMousePosition.y));
             lineRenderer.SetPosition(3, new Vector2(initialMousePosition.x, initialMousePosition.y));
+            lineRenderer.SetPosition(4, lineRenderer.GetPosition(0));
 
             // this boxSelection game object gets a box collider  which is set as a trigger
             // Center of this collider is at BoxSelection position
             boxCollider = gameObject.AddComponent<BoxCollider2D>();
             boxCollider.isTrigger = true;
-            boxCollider.offset = new Vector3(transform.position.x, transform.position.y, transform.position.z); 
+            boxCollider.offset = new Vector2(transform.position.x, transform.position.y); 
+            
         }
+        
+        
 
         // while mouse button is being held down I can draw a rectangle 
         // those four points get corresponding coordinates depending on
         // mouse initial position when button was pressed for the first time
         // and its current position
 
-        if (Input.GetMouseButtonDown(0) && !ObjectSelected.mouseOverObject)
+        
+        if (Input.GetMouseButton(0) && !ObjectSelected.mouseOverObject)
         {
             currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             lineRenderer.SetPosition(0, new Vector2(initialMousePosition.x, initialMousePosition.y));
-            lineRenderer.SetPosition(1, new Vector2(initialMousePosition.x, initialMousePosition.y));
-            lineRenderer.SetPosition(2, new Vector2(initialMousePosition.x, initialMousePosition.y));
-            lineRenderer.SetPosition(3, new Vector2(initialMousePosition.x, initialMousePosition.y));
+            lineRenderer.SetPosition(1, new Vector2(initialMousePosition.x, currentMousePosition.y));
+            lineRenderer.SetPosition(2, new Vector2(currentMousePosition.x, currentMousePosition.y));
+            lineRenderer.SetPosition(3, new Vector2(currentMousePosition.x, initialMousePosition.y));
 
             // BoxSelection gameobjects position is at the middle of the box drawn
 
@@ -59,15 +70,17 @@ public class boxSelection : MonoBehaviour
                 Mathf.Abs(initialMousePosition.x - currentMousePosition.x),
                 Mathf.Abs(initialMousePosition.y - currentMousePosition.y));
         }
+        
 
         // when mouse button is released box is wiped, collider is destroyed
         // and BoxSelection gameobject goes back to the center of the screen
-
+        
         if (Input.GetMouseButtonUp(0))
         {
             lineRenderer.positionCount = 0;
             Destroy(boxCollider);
             transform.position = Vector3.zero;
         }
+        
     }
 }
