262cd1decbf17729403bc8bf70d68996712ea656
H00gy <148908869+H00gy@users.noreply.github.com>
2026-01-17 14:54:31 -0500

    Update checkBoxConfirmation.cs

--- Assets/scripts/checkBoxConfirmation.cs
+++ Assets/scripts/checkBoxConfirmation.cs
@@ -7,32 +7,31 @@ public class checkBoxConfirmation : MonoBehaviour
     public Transform checkBoxes;
     public GameObject fake;
     public GameObject real;
-    bool confirmation;
+    int confirmation = 0;
     
 
     public void onClick()
     {
-
+        
         foreach(Transform checkBoxTransform in checkBoxes) // looks for if checkbox is checked and sets confirmation value
         {
             GameObject childObj = checkBoxTransform.gameObject;
-
-            if (childObj.GetComponentInChildren<Image>().sprite == null) // if no checkmarks
+            Transform grandChildObj = childObj.transform.GetChild(0);
+            
+            if(grandChildObj.GetComponent<Image>().sprite != null)
             {
-                confirmation = true; 
-            }
-            else
-            {
-                confirmation = false;
+                confirmation++;
+                Debug.Log("false");
+                
             }
         }
-        if (confirmation == true && ObjectSelected.currentSelection != null)
+        if (confirmation == 0 && ObjectSelected.currentSelection != null) // real (no checkmarks)
         {
             
             GameObject thisRTag = Instantiate(real);
             thisRTag.transform.parent = ObjectSelected.currentSelection.transform;
         }
-        else if(confirmation == false && ObjectSelected.currentSelection != null)
+        else if(confirmation > 0 && ObjectSelected.currentSelection != null) // fake (at least 1 checkmark)
         {
             GameObject thisFTag = Instantiate(fake);
             thisFTag.transform.parent = ObjectSelected.currentSelection.transform;
