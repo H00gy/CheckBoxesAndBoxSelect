0c4b02586d69f8fa20af28dd871fbfe47c67e2f9
H00gy <148908869+H00gy@users.noreply.github.com>
2026-01-17 15:21:26 -0500

    checkbox comments and finalization

--- Assets/scripts/checkBoxConfirmation.cs
+++ Assets/scripts/checkBoxConfirmation.cs
@@ -13,12 +13,12 @@ public class checkBoxConfirmation : MonoBehaviour
     public void onClick()
     {
         
-        foreach(Transform checkBoxTransform in checkBoxes) // looks for if checkbox is checked and sets confirmation value
+        foreach(Transform checkBoxTransform in checkBoxes) // Looks through Checkboxes
         {
-            GameObject childObj = checkBoxTransform.gameObject;
-            Transform grandChildObj = childObj.transform.GetChild(0);
+            GameObject childObj = checkBoxTransform.gameObject; 
+            Transform grandChildObj = childObj.transform.GetChild(0); // sets grandchild checkmark 
             
-            if(grandChildObj.GetComponent<Image>().sprite != null)
+            if(grandChildObj.GetComponent<Image>().sprite != null) // if there is a checkmark, increase the confirmation value
             {
                 confirmation++;
                 //Debug.Log("false");
@@ -28,19 +28,30 @@ public class checkBoxConfirmation : MonoBehaviour
         if (confirmation == 0 && ObjectSelected.currentSelection != null && ObjectSelected.currentSelection.transform.childCount < 1) // real (no checkmarks)
         {
             Debug.Log("real");
-            GameObject thisRTag = Instantiate(real);
+            GameObject thisRTag = Instantiate(real); // adds real tag (green triangle) 
             thisRTag.transform.parent = ObjectSelected.currentSelection.transform;
-            thisRTag.transform.position = ObjectSelected.currentSelection.transform.position;
+            thisRTag.transform.position = ObjectSelected.currentSelection.transform.position; // locks child position to parent
         }
         else if(confirmation > 0 && ObjectSelected.currentSelection != null && ObjectSelected.currentSelection.transform.childCount < 1) // fake (at least 1 checkmark)
         {
             Debug.Log("fake");
-            GameObject thisFTag = Instantiate(fake);
+            GameObject thisFTag = Instantiate(fake); // adds fake tag (red triangle) 
             thisFTag.transform.parent = ObjectSelected.currentSelection.transform;
             thisFTag.transform.position = ObjectSelected.currentSelection.transform.position;
         }
-        confirmation = 0; // resets confirmation
+        Reset();
     }
-    
+
+    private void Reset() // resets checkmarks to null and confirmation to 0
+    {
+        confirmation = 0; 
+        foreach (Transform checkBoxTransform in checkBoxes) 
+        {
+            GameObject childObj = checkBoxTransform.gameObject;
+            Transform grandChildObj = childObj.transform.GetChild(0); 
+            grandChildObj.GetComponent<Image>().sprite = null;
+        }
+    }
+
 
 }
